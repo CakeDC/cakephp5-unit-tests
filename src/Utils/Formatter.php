d589aa0f1840a9a45484664242768d5cf3e1c4cc
Jorge González <steinkel@gmail.com>
2024-03-05 15:50:01 +0000

    #prepare-baseline add repl console, formatter and math classes

---
+++ src/Utils/Formatter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Utils;
+
+class Formatter
+{
+    /**
+     * Format percentage of won/lost games
+     *
+     * @param int $won won games
+     * @param int $lost lost games
+     * @return string formatted percentage
+     * @throws \OutOfBoundsException
+     */
+    public function formatStatPercentage(int $won, int $lost): string
+    {
+        if ($won < 0 || $lost < 0) {
+            throw new \OutOfBoundsException('Won and lost must not be < 0');
+        }
+        $percentage = $this->roundedPercentage($won, $lost);
+        if ($percentage === 0) {
+            return __('Play more games!');
+        }
+
+        return $percentage . '%';
+    }
+
+    /**
+     * Wrap dependency
+     *
+     * @param $won
+     * @param $lost
+     * @return int
+     */
+    protected function roundedPercentage($won, $lost): int
+    {
+        return Math::roundedPercentage($won, $lost);
+    }
+}
