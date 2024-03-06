f5d482b2fb073c85196db890b7b362a941915231
Jorge González <steinkel@gmail.com>
2024-03-06 11:20:50 +0000

    fix protected

--- templates/Error/error400.php
+++ templates/Error/error400.php
@@ -24,10 +24,6 @@ if (Configure::read('debug')) :
         <?= h($error->queryString) ?>
     </p>
 <?php endif; ?>
-<?php if (!empty($error->params)) : ?>
-    <strong>SQL Query Params: </strong>
-    <?php Debugger::dump($error->params) ?>
-<?php endif; ?>
 
 <?php
     echo $this->element('auto_table_warning');
